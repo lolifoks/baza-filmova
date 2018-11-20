e9916bf45073ed6a26617534d7ecf4cf8de75a0d
Marija Lekic <marija.lekic91@gmail.com>
2018-11-20 20:40:41 +0100

    Updates

---
+++ precache-manifest.2200af9e801dd579da0ab6cc2761c7f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ef3d0743be4e8c3555ad",
+    "url": "/baza-filmova/static/js/runtime~main.ef3d0743.js"
+  },
+  {
+    "revision": "6cb6ecf4957b337331e8",
+    "url": "/baza-filmova/static/js/main.6cb6ecf4.chunk.js"
+  },
+  {
+    "revision": "c1751e2221cfcbbf9cc4",
+    "url": "/baza-filmova/static/js/1.c1751e22.chunk.js"
+  },
+  {
+    "revision": "6cb6ecf4957b337331e8",
+    "url": "/baza-filmova/static/css/main.9819947b.chunk.css"
+  },
+  {
+    "revision": "caba8892987053a3de1c69a794feee09",
+    "url": "/baza-filmova/index.html"
+  }
+];
